ebab72c9458983706e8954fa87cc69da3f5fe35b
sahil-903 <69524898+sahil-903@users.noreply.github.com>
2022-12-27 13:12:36 +0530

    Create Implementing Dijkstra Algorithm using set.cpp

---
+++ Implementing Dijkstra Algorithm using set.cpp
@@ -0,0 +1,91 @@
+// dijakstra's algorithm using set data struture as it contain erase operation.
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Create a set ds for storing the nodes as a pair {dist,node}
+        set<pair<int,int>> st; 
+        vector<int> dist(V, 1e9); 
+        
+        st.insert({0, S}); 
+
+        // Source initialised with dist=0
+        dist[S] = 0;
+        
+        // Now, erase the minimum distance node first from the set
+        // and traverse for all its adjacent nodes.
+        while(!st.empty()) {
+            auto it = *(st.begin()); 
+            int node = it.second; 
+            int dis = it.first; 
+            st.erase(it); 
+            
+            // Check for all adjacent nodes of the erased
+            // element whether the prev dist is larger than current or not.
+            for(auto it : adj[node]) {
+                int adjNode = it[0]; 
+                int edgW = it[1]; 
+                
+                if(dis + edgW < dist[adjNode]) {
+                    // erase if it was visited previously at 
+                    // a greater cost.
+                    if(dist[adjNode] != 1e9) 
+                        st.erase({dist[adjNode], adjNode}); 
+                        
+                    // If current distance is smaller,
+                    // push it into the queue
+                    dist[adjNode] = dis + edgW; 
+                    st.insert({dist[adjNode], adjNode}); 
+                 }
+            }
+        }
+        return dist; 
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
