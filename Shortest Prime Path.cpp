95fb66e11b82643c46f7db8e7cb09825f8c593b2
sahil-903 <69524898+sahil-903@users.noreply.github.com>
2023-01-06 09:30:46 +0530

    Create Shortest Prime Path.cpp

---
+++ Shortest Prime Path.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{   
+  public:
+  bool isPrime(int n) {
+        if(n==0 or n==1) 
+            return false;
+        for(int i=2;i<=sqrt(n);i++) 
+        {
+            if(n%i == 0) 
+                return false;
+        }
+        return true;
+    }
+    int shortestPath(int Num1,int Num2)
+    {   
+        // Complete this function using prime vector
+        string s=to_string(Num1);
+        string s1=to_string(Num2);
+        queue<pair<string,int>> q;
+        q.push({s,0});
+        vector<int> vis(1e5);
+        while(!q.empty())
+        {
+            string word=q.front().first;
+            int steps=q.front().second;
+            q.pop();
+            vis[stoi(word)]=1;
+            if(word==s1)
+                return steps;
+            for(int i=0;i<word.size();i++)
+            {
+                char ch=word[i];
+                for(int j=0;j<10;j++)
+                {
+                    if(i==0 && j==0)
+                        continue;
+                    word[i]=char(j+'0');
+                    int val=stoi(word);
+                    if(isPrime(val) && !vis[val])
+                    {
+                        vis[val]=1;
+                        q.push({word,steps+1});
+                    }
+                }
+                word[i]=ch;
+            }
+        }
+        return -1;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+signed main()
+{
+    int t;
+    cin>>t;
+    Solution obj;
+    while(t--)
+    {
+        int Num1,Num2;
+        cin>>Num1>>Num2;
+        int answer=obj.shortestPath(Num1,Num2);
+        cout<<answer<<endl;
+    }
+}
+// } Driver Code Ends
